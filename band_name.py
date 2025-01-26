efa69380bb9f7d1eb72d11a58daec8898f4ebc2c
adapmas <samster.co19@gmail.com>
2025-01-27 03:02:46 +0530

    Created band_name.py

---
+++ band_name.py
@@ -0,0 +1,4 @@
+print("Welcome to the BAND NAME GENERATOR!!!")
+city=input("What's your city's name?: ")
+animal=input("What's the name of your favourite animal(in plural)?: ")
+print("The name of your band is:\n"+city+" "+animal)
