774bb11b9d3bc160cb5192ff838065efc636c0b5
adapmas <samster.co19@gmail.com>
2025-01-30 00:34:39 +0530

    Create random_int_float.py

---
+++ random_int_float.py
@@ -0,0 +1,10 @@
+import random
+
+i=random.randint(0,100)
+print(i) #79
+j=random.random()
+print(j) #0.06593548840283758
+k=random.random()*20
+print(k) #9.395819440273158
+l=random.uniform(0,12)
+print(l) #2.272060260573189
