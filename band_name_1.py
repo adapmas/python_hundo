13c362eeaacc0ea5293c2166f6811352c2fece68
adapmas <samster.co19@gmail.com>
2025-01-29 11:47:12 +0530

    Delete band_name_1.py

--- band_name_1.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the BAND NAME GENERATOR!!!")
-city=input("What's your city's name?: ")
-animal=input("What's the name of your favourite animal(in plural)?: ")
-print("The name of your band is:\n"+city+" "+animal)
